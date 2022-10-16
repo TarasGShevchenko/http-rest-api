4d70b2f6148c22b91c661c69350d7993a7e35eb7
Taras Shechenko <tshevchenko@rightandabove.com>
2022-10-16 16:34:09 +0300

    Initial commit

---
+++ internal/app/model/user.go
@@ -0,0 +1,53 @@
+package model
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID                int    `json:"id"`
+	Email             string `json:"email"`
+	Password          string `json:"password,omitempty"`
+	EncryptedPassword string `json:"-"`
+}
+
+func (u *User) Validate() error {
+	return validation.ValidateStruct(
+		u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.By(requiredIf(u.EncryptedPassword == "")), validation.Length(6, 100)),
+	)
+}
+
+func (u *User) BeforeCreate() error {
+	if len(u.Password) > 0 {
+		enc, err := encriptString(u.Password)
+		if err != nil {
+			return err
+		}
+
+		u.EncryptedPassword = enc
+	}
+
+	return nil
+}
+
+func (u *User) Sanitize() {
+	u.Password = ""
+}
+
+func (u *User) ComparePassword(pass string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(u.EncryptedPassword), []byte(pass)) == nil
+}
+
+func encriptString(s string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
+}
